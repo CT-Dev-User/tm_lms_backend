2637c504141428d6df4e5923834b003d27f81232
Conscientious Technologies <167194750+CT-Dev-User@users.noreply.github.com>
2025-04-16 16:18:26 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,10 +1,21 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const conn = async() =>{
-    try{
-      await mongoose.connect(process.env.DB)
-      console.log("database sucess")
-    }catch(error){
-        console.log(error);
-    }
-}
+// export const conn = async() =>{
+//     try{
+//       await mongoose.connect(process.env.DB)
+//       console.log("database sucess")
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+
+export const conn = async () => {
+  try {
+    console.log("Connecting to DB:", process.env.DB);
+    await mongoose.connect(process.env.DB);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.log("DB Connection Error:", error);
+  }
+};
